10c644b50e569bb1e69b4c9c3b99a4e6d830b6ec
rrauf8178 <rrauf8178@gmail.com>
2024-01-06 22:37:54 +0400

    finish product cart

--- src/components/productCart/ProductCart.js
+++ src/components/productCart/ProductCart.js
@@ -1,9 +1,36 @@
-import React from 'react';
-import "./ProductCart.css"
 
-const ProductCart = () => {
+import "./ProductCart.css"
+import { useContext } from "react";
+import Rating from "../Rating";
+import CartContext from "../../context/cart/CartContext";
+import formatCurrency from "format-currency";
+
+const ProductCart = ({product}) => {
+  const {addToCart} = useContext(CartContext);
+  let opts = { format: "%s%v", symbol: "€" };
+
   return (
-    <div>ProductCart</div>
+    <div className='productCard__wrapper'>
+    <div>
+      <img className='productCard__img' src={product.image} alt='' />
+      <h4>{product.name}</h4>
+      <div className='ProductCard__price'>
+        <h5>{formatCurrency(`${product.price}`, opts)}</h5>
+      </div>
+      <div className='ProductCard__Rateing'>
+        <Rating
+          value={product.rating}
+          text={`${product.numReviews} reviews`}
+        />
+      </div>
+      <button
+        className='ProductCard__button'
+        onClick={() => addToCart(product)}
+      >
+        Add to basket
+      </button>
+    </div>
+  </div>
   )
 }
 
