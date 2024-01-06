00eb2493a96a450c87d51d60284f5ef3c3624d61
rrauf8178 <rrauf8178@gmail.com>
2024-01-06 22:59:54 +0400

    finish home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,15 @@
-import React from 'react';
-import "./Home.css"
+import "./Home.css";
+import products from "../data";
+import ProductCart from "../components/productCart/ProductCart";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div className="products__wrapper">
+      {products.map((product) => (
+        <ProductCart key={product.id} product={product} />
+      ))}
+    </div>
+  );
+};
 
-export default Home
+export default Home;
